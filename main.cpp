9a6e50bb5c12ef0804cb9cb870a29d52c5af2286
malhadbah <malhadbah@mail.csuchico.edu>
2017-10-23 10:25:36 -0700

    first greeting

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+//special greeting 
+//prototype
+void greeting();
+
+int main()
+{
+//need to specify everything
+//function call
+greeting();
+
+return 0;
+}
+// function greeting user
+void greeting()
+{
+cout<<"welcome"<<endl;
+}
+
